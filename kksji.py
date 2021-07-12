27068a94f66ba5eac9e0aa66fdc1de7ae1804c83
mensider <soorajneeraja@gmail.com>
2021-07-12 10:26:31 +0530

    Arena Image added

---
+++ kksji.py
@@ -0,0 +1,7 @@
+# The Plan
+# --------
+# [.] Use an image of the arean as background
+# [.] Define 4 rect blocks, each for a bot
+# [.] Assume a precursor fn which will provide coordinates of each bot
+
+# [.] Go Plan Epsilon
