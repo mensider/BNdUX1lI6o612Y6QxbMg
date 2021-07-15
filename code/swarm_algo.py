746aa3e5f7008cee66127f33a131bd0051fc809d
mensider <soorajneeraja@gmail.com>
2021-07-16 00:07:07 +0530

    Grid for Round 1
    Grid labelled
    Implemented in Python using networkx
    Now mapping movements into nodes

--- code/swarm_algo.py
+++ code/swarm_algo.py
@@ -1,15 +1,14 @@
 #fn signature
 #[bot1_dx,bot1_dy, bot2_dx,bot2_dy, bot3_dx,bot3_dy, bot4_dx,bot4_dy] = swarm_algo(bot1_x,bot1_y , bot2_x,bot2_y , bot3_x,bot3_y , bot4_x,bot4_y)
-
 def swarm_algo(bot1_x,bot1_y , bot2_x,bot2_y , bot3_x,bot3_y , bot4_x,bot4_y):
 
 
-    bot1_dx = 0;
-    bot1_dy = -10;
-    bot2_dx = 0;
-    bot2_dy = -10;
-    bot3_dx = 0;
-    bot3_dy = -10;
-    bot4_dx = 0;
-    bot4_dy = -10;
+    bot1_dx = 0
+    bot1_dy = -10
+    bot2_dx = 0
+    bot2_dy = -10
+    bot3_dx = 0
+    bot3_dy = -10
+    bot4_dx = 0
+    bot4_dy = -10
     return (bot1_dx,bot1_dy, bot2_dx,bot2_dy, bot3_dx,bot3_dy, bot4_dx,bot4_dy)
